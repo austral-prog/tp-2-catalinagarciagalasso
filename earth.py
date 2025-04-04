75bf7819dd6d6e60b6e2bdfcbb63d8dc4428a9f6
catalinagarciagalasso <cgarciagalasso@mail.austral.edu.ar>
2025-04-04 11:20:16 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -1,3 +1,7 @@
 def earth():
     x = "Bangladesh"
     y = "Barbados"
+    resultado = "x" < "y"
+    print("The result of X comes first in the dictionary than Y is", resultado)
+    resultado2 = "y" < "x"
+    print("The result of Y comes first in the dictionary than X is", resultado2)
